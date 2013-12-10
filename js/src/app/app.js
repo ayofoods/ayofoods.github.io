d7ee0d1aea6582a7932017de72a9120b0d4a520a
Daniel Kwiecinski <daniel@opengamma.com>
2013-12-10 22:53:07 +0000

    5

--- js/src/app/app.js
+++ js/src/app/app.js
@@ -1,5 +1,6 @@
 angular.module( 'cabmini', [
   'firebase',
+  'directives.datatable',
   'ui.mask'//,
   //'templates-app',
   //'templates-common',
@@ -19,15 +20,21 @@ angular.module( 'cabmini', [
 .run( function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $http, angularFire, angularFireAuth, angularFireCollection, $location) {
+.controller( 'AppCtrl', function AppCtrl ( $compile, $scope, $http, angularFire, angularFireAuth, angularFireCollection, $location) {
 
   $scope.basket = {};
 
   var ref = new Firebase("https://ayofoods.firebaseio.com/");
   angularFireAuth.initialize(ref, {scope: $scope, name: "user"});
 
-  var inventory = new Firebase("https://ayofoods.firebaseio.com/inventory");
-  angularFire(inventory, $scope, 'items');  
+  angularFireCollection(new Firebase("https://ayofoods.firebaseio.com/inventory"), function(data){
+    $scope.items = data.val();
+    $scope.items_index = _.reduce($scope.items, function(acc, i){
+      acc[i.id] = i;
+      return acc;
+    }, {});
+  });
+  //angularFire(inventory, $scope, 'items');  
 
   // $http.get('/ayofoods-export.json').success(function(data) {
   //   $scope.items = _.values(data.inventory);
@@ -103,7 +110,8 @@ angular.module( 'cabmini', [
   });
 
 
-  $scope.incX = function(item) {
+  $scope.incX = function(id) {
+    var item = $scope.items_index[id];
     $scope.basket = $scope.basket || {};
     $scope.basket[item.id] = $scope.basket[item.id] || {};
     var qty = $scope.basket[item.id].qty || 0;
@@ -112,6 +120,7 @@ angular.module( 'cabmini', [
   };
 
   $scope.decX = function(id) {
+    var item = $scope.items_index[id];
     $scope.basket = $scope.basket || {};
     $scope.basket[item.id] = $scope.basket[item.id] || {};
     var qty = $scope.basket[item.id].qty || 0;
@@ -121,74 +130,88 @@ angular.module( 'cabmini', [
     }
   };
 
-  // GoogleMapsApi.then(
-  //   function(google) {
-  //     google.maps.visualRefresh = true;
-  //     var mapOptions = {
-  //       scrollwheel: false,
-  //       panControl: false,
-  //       zoomControl: false,
-  //       mapTypeControl: false,
-  //       scaleControl: false,
-  //       streetViewControl: false,
-  //       overviewMapControl: false,  
-  //       disableDefaultUI: true,
-  //       disableDoubleClickZoom: true,
-  //       draggable: false,
-  //       keyboardShortcuts: false,
-  //       mapTypeId: google.maps.MapTypeId.SATELLITE,
-  //       tilt: 45,
-  //       zoom: 3,
-  //       center: new google.maps.LatLng(0, 25),
-  //       styles: [
-  //         // {
-  //         //   "featureType": "landscape",
-  //         //   "elementType": "geometry",
-  //         //   "stylers": [
-  //         //     { "visibility": "off" }
-  //         //   ]
-  //         // },{
-  //         //   "featureType": "poi",
-  //         //   "stylers": [
-  //         //     { "visibility": "off" }
-  //         //   ]
-  //         // },{
-  //         //   "featureType": "water",
-  //         //   "stylers": [
-  //         //     { "visibility": "on" },
-  //         //     { "lightness": -88 }
-  //         //   ]
-  //         // },{
-  //         //   "featureType": "road.local",
-  //         //   "stylers": [
-  //         //     { "lightness": -72 }
-  //         //   ]
-  //         // },{
-  //         //   "featureType": "road.arterial",
-  //         //   "stylers": [
-  //         //     { "lightness": -74 },
-  //         //     { "color": "#f38080" },
-  //         //     { "weight": 0.6 }
-  //         //   ]
-  //         // },{
-  //         //   "featureType": "road.highway",
-  //         //   "stylers": [
-  //         //     { "lightness": 34 },
-  //         //     { "color": "#ffffff" }
-  //         //   ]
-  //         // }
-  //       ]
-  //     };
-  //     $scope.MyMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  //   }, 
-  //   function(reason) {
-  //     alert('Failed: ' + reason);
-  //   }, 
-  //   function(update) {
-  //     alert('Got notification: ' + update);
-  //   });
+  $.fn.dataTableExt.sErrMode = 'throw';
 
-  
+  $scope.tblColumns = [
+
+    {
+      sTitle: "",
+      //mData: ".",
+      mData: function (source, type, val) {
+        return source;
+      },
+      mRender: function (item) {
+        return '<img src="'+item.flickr+'" height="50">';
+      }
+    },
+    {
+      sTitle: "Description",
+      //mData: ".",
+      mData: function (source, type, val) {
+        return source;
+      },
+      mRender: function (item) {
+        return item.desc || '';
+      }
+    },
+    {
+      sTitle: "Content",
+      //mData: ".",
+      mData: function (source, type, val) {
+        return source;
+      },
+      mRender: function (item) {
+        return item.desc || '';
+      }
+
+    },
+    {
+      sTitle: "In your basket",
+      //mData: ".",
+      mData: function (source, type, val) {
+        return source;
+      },
+      mRender: function (item) {
+        return '<strong ng-bind="basket[&apos;'+item.id+'&apos;].qty"></strong>'+
+        '<a class="btn btn-primary pull-right" ng-click="decX(&apos;'+item.id+'&apos;)"><i class="fa fa-minus-circle"></i></a>'+
+        '<span class="pull-right">&nbsp;</span>'+
+        '<a class="btn btn-primary pull-right" ng-click="incX(&apos;'+item.id+'&apos;)"><i class="fa fa-plus-circle"></i></a>'
+        ;
+      }
+
+    }    
+  ];
+
+  $scope.overrideOptions = {
+    "sDom": "<'row'<'col-xs-6'l><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>",
+    "sPaginationType": "bootstrap",
+    "oLanguage": {
+            "sLengthMenu": "_MENU_ records per page"
+    },
+    // sDom: "<'row-fluid'" +
+    //     "  <'span6'l>" +
+    //     "  <'span6'" +
+    //     "    <'input-prepend'" +
+    //     "      <'add-on'" +
+    //     "        <'icon-search'>" +
+    //     "      >" +
+    //     "      f" +
+    //     "    >" +
+    //     "  r>" +
+    //     "" +
+    //     "    t" +
+    //     "    <'row-fluid'" +
+    //     "      <'span6'i>" +
+    //     "      <'span6'p>>",
+    // sPaginationType: "bootstrap",
+    // oLanguage: {
+    //   sSearch: ""
+    // },
+    fnCreatedRow: function (nRow, aData, iDataIndex) {
+      //$(nRow).attr('ng-click', 'show("' + aData._id + '")');
+      $compile(nRow)($scope);
+    }
+  };
   
 });
 
@@ -209,4 +232,165 @@ jQuery('.tooltip').on('mouseover', function(){
   // }); 
   console.log("tooltip");
 });
+
+
+
+
+
+
+
+
+
+/* Set the defaults for DataTables initialisation */
+$.extend( true, $.fn.dataTable.defaults, {
+        "sDom": "<'row'<'col-xs-6'l><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+                "sLengthMenu": "_MENU_ records per page"
+        }
+} );
+
+
+
+
+/* Default class modification */
+$.extend( $.fn.dataTableExt.oStdClasses, {
+        "sWrapper": "dataTables_wrapper form-inline",
+        "sFilterInput": "form-control input-sm",
+        "sLengthSelect": "form-control input-sm"
+} );
+
+
+/* API method to get paging information */
+$.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
+{
+        return {
+                "iStart":         oSettings._iDisplayStart,
+                "iEnd":           oSettings.fnDisplayEnd(),
+                "iLength":        oSettings._iDisplayLength,
+                "iTotal":         oSettings.fnRecordsTotal(),
+                "iFilteredTotal": oSettings.fnRecordsDisplay(),
+                "iPage":          oSettings._iDisplayLength === -1 ?
+                        0 : Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
+                "iTotalPages":    oSettings._iDisplayLength === -1 ?
+                        0 : Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
+        };
+};
+
+
+/* Bootstrap style pagination control */
+$.extend( $.fn.dataTableExt.oPagination, {
+        "bootstrap": {
+                "fnInit": function( oSettings, nPaging, fnDraw ) {
+                        var oLang = oSettings.oLanguage.oPaginate;
+                        var fnClickHandler = function ( e ) {
+                                e.preventDefault();
+                                if ( oSettings.oApi._fnPageChange(oSettings, e.data.action) ) {
+                                        fnDraw( oSettings );
+                                }
+                        };
+
+                        $(nPaging).append(
+                                '<ul class="pagination">'+
+                                        '<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
+                                        '<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
+                                '</ul>'
+                        );
+                        var els = $('a', nPaging);
+                        $(els[0]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
+                        $(els[1]).bind( 'click.DT', { action: "next" }, fnClickHandler );
+                },
+
+                "fnUpdate": function ( oSettings, fnDraw ) {
+                        var iListLength = 5;
+                        var oPaging = oSettings.oInstance.fnPagingInfo();
+                        var an = oSettings.aanFeatures.p;
+                        var i, ien, j, sClass, iStart, iEnd, iHalf=Math.floor(iListLength/2);
+
+                        if ( oPaging.iTotalPages < iListLength) {
+                                iStart = 1;
+                                iEnd = oPaging.iTotalPages;
+                        }
+                        else if ( oPaging.iPage <= iHalf ) {
+                                iStart = 1;
+                                iEnd = iListLength;
+                        } else if ( oPaging.iPage >= (oPaging.iTotalPages-iHalf) ) {
+                                iStart = oPaging.iTotalPages - iListLength + 1;
+                                iEnd = oPaging.iTotalPages;
+                        } else {
+                                iStart = oPaging.iPage - iHalf + 1;
+                                iEnd = iStart + iListLength - 1;
+                        }
+
+                        for ( i=0, ien=an.length ; i<ien ; i++ ) {
+                                // Remove the middle elements
+                                $('li:gt(0)', an[i]).filter(':not(:last)').remove();
+
+                                // Add the new list items and their event handlers
+                                /*jshint -W083 */
+                                for ( j=iStart ; j<=iEnd ; j++ ) {
+                                        sClass = (j==oPaging.iPage+1) ? 'class="active"' : '';
+                                        $('<li '+sClass+'><a href="#">'+j+'</a></li>')
+                                                .insertBefore( $('li:last', an[i])[0] )
+                                                .bind('click', function (e) {
+                                                        e.preventDefault();
+                                                        oSettings._iDisplayStart = (parseInt($('a', this).text(),10)-1) * oPaging.iLength;
+                                                        fnDraw( oSettings );
+                                                });
+                                }
+
+                                // Add / remove disabled classes from the static elements
+                                if ( oPaging.iPage === 0 ) {
+                                        $('li:first', an[i]).addClass('disabled');
+                                } else {
+                                        $('li:first', an[i]).removeClass('disabled');
+                                }
+
+                                if ( oPaging.iPage === oPaging.iTotalPages-1 || oPaging.iTotalPages === 0 ) {
+                                        $('li:last', an[i]).addClass('disabled');
+                                } else {
+                                        $('li:last', an[i]).removeClass('disabled');
+                                }
+                        }
+                }
+        }
+} );
+
+
+/*
+ * TableTools Bootstrap compatibility
+ * Required TableTools 2.1+
+ */
+if ( $.fn.DataTable.TableTools ) {
+        // Set the classes that TableTools uses to something suitable for Bootstrap
+        $.extend( true, $.fn.DataTable.TableTools.classes, {
+                "container": "DTTT btn-group",
+                "buttons": {
+                        "normal": "btn btn-default",
+                        "disabled": "disabled"
+                },
+                "collection": {
+                        "container": "DTTT_dropdown dropdown-menu",
+                        "buttons": {
+                                "normal": "",
+                                "disabled": "disabled"
+                        }
+                },
+                "print": {
+                        "info": "DTTT_print_info modal"
+                },
+                "select": {
+                        "row": "active"
+                }
+        } );
+
+        // Have the collection use a bootstrap compatible dropdown
+        $.extend( true, $.fn.DataTable.TableTools.DEFAULTS.oTags, {
+                "collection": {
+                        "container": "ul",
+                        "button": "li",
+                        "liner": "a"
+                }
+        } );
+}
 
